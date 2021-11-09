ed2c484cf6cc324f4d9e5fbcfc8fbddb0f87c699
poncelettheo <theo.poncelet99@gmail.com>
2021-11-09 17:43:39 +0100

    scrolling_screen

---
+++ Assets/screen_height.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screen_height : MonoBehaviour
+{
+    private RectTransform panelRectTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelRectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(panelRectTransform.rect.height);
+        Debug.Log(panelRectTransform.rect.width);
+    }
+}
